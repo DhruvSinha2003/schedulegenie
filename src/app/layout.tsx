b5d505bcc23567e14b84625b632878b3aef54734
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-04-12 19:32:13 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 // app/layout.tsx
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ScheduleGenie",
   description: "AI Powered Scheduling Assistant",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
